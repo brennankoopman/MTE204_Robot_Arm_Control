86f0e7e174069ad0ca293cfa52a4aad3596b220e
JordanSlater <32441504+JordanSlater@users.noreply.github.com>
2017-10-13 21:27:23 -0400

    FIXED THE THING
    it now does wat it gotta do!

--- Jacobian.m
+++ Jacobian.m
@@ -4,19 +4,33 @@ function J = Jacobian(q)
 
 len = [0 1 1];
 
-%q = [0;0;0];
 
+
+%{
 dx1 = -(len(2)*cos(q(2))+len(3)*cos(q(2)-q(3)))*sin(q(1));
-dx2 = (-len(2)*sin(q(2))-sin(q(2)-q(3)))*cos(q(1));
-dx3 = (len(2)*cos(q(2))-sin(q(2)-q(3)))*cos(q(1));
+dx2 = (-len(2)*sin(q(2))-len(3)*sin(q(2)-q(3)))*cos(q(1));
+dx3 = (-len(3)*sin(q(2)-q(3)))*cos(q(1));
 
 dy1 = (len(2)*cos(q(2))+len(3)*cos(q(2)-q(3)))*cos(q(1));
-dy2 = (-len(2)*sin(q(2))-sin(q(2)-q(3)))*cos(q(1));
-dy3 = (len(2)*cos(q(2))-sin(q(2)-q(3)))*cos(q(1));
+dy2 = (-len(2)*sin(q(2))-len(3)*sin(q(2)-q(3)))*sin(q(1));
+dy3 = (-len(3)*sin(q(2)-q(3)))*sin(q(1));
 
-dz1 = len(2)*sin(q(2))+len(3)*sin(q(2)-q(3));
+dz1 = 0;
 dz2 = len(2)*cos(q(2))+len(3)*cos(q(2)-q(3));
-dz3 = len(2)*sin(q(2))-len(3)*cos(q(2)-q(3));
+dz3 = -len(3)*cos(q(2)-q(3));
+%}
+
+dx1 = -(len(2)*cos(q(2)) + len(3)*cos(q(2)-q(3)))*sin(q(1));
+dx2 = (-len(2)*sin(q(2))-len(3)*sin(q(2)-q(3)))*cos(q(1));
+dx3 = (len(3)*sin(q(2)-q(3)))*cos(q(1));
+
+dy1 = (len(2)*cos(q(2))+len(3)*cos(q(2)-q(3)))*cos(q(1));
+dy2 = (-len(2)*sin(q(2))-len(3)*sin(q(2)-q(3)))*sin(q(1));
+dy3 = len(3)*sin(q(2)-q(3))*sin(q(1));
+
+dz1 = 0;
+dz2 = len(2)*cos(q(2))+len(3)*cos(q(2)-q(3));
+dz3 = -len(3)*cos(q(2)-q(3));
 
 J = [dx1,dx2,dx3;
      dy1,dy2,dy3;
