500e60f8e04cc6f7fab17d3f3baefd3f4a8ec341
Brennan Koopman <brennankoopman@gmail.com>
2017-11-03 08:30:11 -0400

    time analsysis
    most of the changed code are in copied file in the new folder
    some of the main files i made some QOL changes

--- armLinMoveScript.m
+++ armLinMoveScript.m
@@ -1,15 +1,14 @@
 more off;
-q = [0;0;-45];
+q = [75;0;40];
 q = q*(pi/180);
-t = [-1.6;0.3;-0.3];
+t = [0.2;-1;1.5];
 
 Q = [q];    %list of all angles for each target point starting at the initial position
 
-n=1;
-Points = armLinMove(t,q,n)
+Points = armLinMove(t,q);
 
 
-n = n+1;
+n = size(Points,2);
 
   %CALCULATES THE ANGLES FOR EACH TARGET POINT USING THE JACOBIAN METHOD
   for a = 1:n-1
@@ -17,41 +16,43 @@ n = n+1;
   end
   
  
- cPoints = [Points(:,1)] %list of points, with 100 points for each arc, ie target point, here we just set the first entry the initial position
-  
-  for a = 1:n-1                      %for each jump in angles
+ cPoints = [Points(:,1)]; %list of points, with 100 points for each arc, ie target point, here we just set the first entry the initial position
+ 
+  for a = 1:n-1                       %for each jump in angles
     
-    dQ = Q(:,a+1) - Q(:,a) ;         %finds the change in angle needed for this step
+    dQ = Q(:,a+1) - Q(:,a) ;          %finds the change in angle needed for this step
     
+    %it turns out that the rotations dont linearlize nicely when a path crosses
+    %the refrence axis, since the change in angle from 350 -> 10 should be 20, 
+    % NOT -340, thus a single dq value cannot exceed 180 deg, as the smaller 
+    % rotation value is preffered. note that angles are in RADIANS
     
-    if( abs(dQ(1)) > pi ) 
-      dQ(1) = -sign(dQ(1))*2*pi + dQ(1);
+    if( abs(dQ(1))>pi) 
+     dQ(1) = dQ(1)-2*pi*sign(dQ(1));  %takes the opposite of the sign of your dQ * 360, and adds your dQ to find your opposite rotation
     end
     
-    if( abs(dQ(2)) > pi ) 
-      dQ(2) = -sign(dQ(2))*2*pi + dQ(2);
+    if( abs(dQ(2))>pi) 
+     dQ(2) = dQ(2)-2*pi*sign(dQ(2)); 
+    end 
+    
+    if( abs(dQ(3))>pi) 
+     dQ(3) = dQ(3)-2*pi*sign(dQ(3));  %takes the opposite of the sign of your dQ * 360, and adds your dQ to find your opposite rotation
     end
-    
-    if( abs(dQ(3)) > pi ) 
-      dQ(3) = -sign(dQ(2))*2*pi + dQ(3);
-    end
-    
-    
-    dq = dQ/30;                 %these are the incremental changes to q to show that the end effector does not move linearly
+     
+    dq = dQ/40;                     %these are the incremental changes to q to show that the end effector does not move linearly
    
     
-    for b = 1:30                    %find 100 points on the arc
+    for b = 1:40                   %find 100 points on the arc
       nu = armFunction( ( Q(:,a) + b*dq) , [0;0;0] );
       cPoints = [cPoints, nu];
     end
    
   end
-  %cPoints
 
-plot3(cPoints(1,:),cPoints(2,:),cPoints(3,:),'r'); %plotting the endpoint between each point
-
-hold on;  
-plot3(Points(1,:),Points(2,:),Points(3,:),'g');   %plotting the endpoint @ each target point
+hold on;
+plot3(cPoints(1,:),cPoints(2,:),cPoints(3,:),'r'); %plotting the true path between each point
+ 
+plot3(Points(1,:),Points(2,:),Points(3,:),'g');   %plotting the desired path @ with each target point
 
 xlim([-2 2]);
 ylim([-2 2]);
